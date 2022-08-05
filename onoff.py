9c2f33b930988f7889fe8ee9c097148930f9f2b1
Cbasjax2022 <j.odaniel2@students.clark.edu>
2022-08-05 14:41:20 -0700

    V2

--- onoff.py
+++ onoff.py
@@ -12,13 +12,22 @@ import subprocess
 from datetime import datetime
 import gspread
 
-gc = gspread.service_account(filename='MMM PIR data')
+# DEFINE VARIABLES
+# SECRET CREDENTIALS FILE
+gc = gspread.service_account(filename='apicredentials.json')
+# API KEY
 sh = gc.open_by_key('1DorK1dQWwjlAj4ZiZ9OE_o8lVTSwrah3tS0H1yJ9ctc')
-worksheet = sh.sheet1
-res = worksheet.get_all_records()
-print(res)
 
+worksheet = sh.sheet1
 now = datetime.now()
+
+# Function to find next empty row in spreadsheet
+
+
+def next_available_row(worksheet):
+    str_list = list(filter(None, worksheet.col_values(1)))
+    return str(len(str_list)+1)
+
 
 RED_PIN = 17
 GREEN_PIN = 22
@@ -41,10 +50,16 @@ def setLights(pin, brightness):
 tmp = os.popen("sudo /opt/vc/bin/tvservice -s").read()
 if tmp.find("off") == -1:
     print("monitor is on")
-    # this should print out the current time
+    next_row = next_available_row(worksheet)
     current_datetime = now.strftime("%Y-%m-%d %H:%M:%S")
-    # writes current_date time to google sheet
-    worksheet.update_cell(current_datetime)
+
+    # update_acell uses "a1, b7,etc cell notation.
+    # update_cell uses "1, 1" cell notation as in 1st row 1st column.
+    # The following line updates the A column and uses string substitution to \
+    # dynamically assign the next available row in the sheet
+    worksheet.update_acell("A{}".format(next_row), current_datetime)
+    # diagnostic output
+    print("wrote {} row to spreadsheet".format(i+1))
 
 
 else:
